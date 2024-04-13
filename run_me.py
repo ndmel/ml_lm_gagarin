cfa9b32c0dbd5880b4faed21918584b418c47b2d
Aleksey Zubaryov <aleksey.zubaryov@leroymerlin.ru>
2024-04-13 22:19:18 +0300

    Mypy fix

--- run_me.py
+++ run_me.py
@@ -14,9 +14,9 @@ PATH_TO_SENTIMENT_MODEL = pathlib.Path('model') / 'clf_sentiment.pkl'
 
 
 def load_artifacts(
-    vector_path: pathlib.PosixPath = PATH_TO_VECTORIZER,
-    company_model_path: pathlib.PosixPath = PATH_TO_COMPANY_MODEL,
-    sentiment_model_path: pathlib.PosixPath = PATH_TO_SENTIMENT_MODEL
+    vector_path: pathlib.Path = PATH_TO_VECTORIZER,
+    company_model_path: pathlib.Path = PATH_TO_COMPANY_MODEL,
+    sentiment_model_path: pathlib.Path = PATH_TO_SENTIMENT_MODEL
 ):
 
     vectorizer = joblib.load(vector_path)
@@ -26,14 +26,14 @@ def load_artifacts(
     return vectorizer, clf_company, clf_sentiment
 
 
-def load_data(path: pathlib.PosixPath = PATH_TO_TEST_DATA) -> tp.List[str]:
+def load_data(path: pathlib.Path = PATH_TO_TEST_DATA) -> tp.List[str]:
     with open(path, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
     return data
 
 
-def save_data(data, path: pathlib.PosixPath = PATH_TO_OUTPUT_DATA):
+def save_data(data, path: pathlib.Path = PATH_TO_OUTPUT_DATA):
     with open(path, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=1, ensure_ascii=False)
 
